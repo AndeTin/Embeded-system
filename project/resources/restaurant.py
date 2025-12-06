c6b21eacb4bd143d3e44db5c0b63345ec8e31def
Abby <B1129006@cgu.edu.tw>
2025-12-07 03:22:20 +0800

    restaurant search by name and location

--- project/resources/restaurant.py
+++ project/resources/restaurant.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
-from models import Restaurant
-from app import db
+# from models import Restaurant
+
 
 class RestaurantListResource(Resource):
     def get(self):
@@ -11,3 +11,57 @@ class RestaurantResource(Resource):
     def get(self, restaurant_id):
         restaurant = Restaurant.query.get_or_404(restaurant_id)
         return restaurant.as_dict(), 200
+    
+import os
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+
+def get_db_engine():
+    load_dotenv()
+    database_url = os.getenv("DATABASE_URL")
+
+    if not database_url:
+        raise ValueError("DATABASE_URL not set in .env file")
+    
+    engine = create_engine(database_url)
+    return engine
+
+# 餐廳名稱搜尋
+def restaurant_name(name):
+    try:
+        engine = get_db_engine()
+        with engine.connect() as connection:
+            result = connection.execute(
+                text("""SELECT name FROM restaurants WHERE name LIKE :name"""), {"name": f"%{name}%"}
+            )
+            rows = result.fetchall()
+
+            print("Database成功")
+            print("Name Result: ")
+            for row in rows:
+                print(row)
+            print("\n")
+
+    except Exception as e:
+        print("Database connection failed: ", e)
+
+restaurant_name('火鍋')
+
+# 餐廳地點搜尋
+def restaurant_location(address):
+    try:
+        engine = get_db_engine()
+        with engine.connect() as connection:
+            result = connection.execute(
+                text("""SELECT name FROM restaurants WHERE address LIKE :address"""), {"address": f"%{address}%"}
+            )
+            rows =  result.fetchall()
+
+            print('Location Result: ')
+            for row in rows:
+                print(row)
+
+    except Exception as e:
+        print("Database connection failed: ", e)
+
+restaurant_location('板橋')
